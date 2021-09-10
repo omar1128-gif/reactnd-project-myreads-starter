26d9564aca704357db81ff6d5b72b7372bd9429f
omar1128-gif <o_fathy28@yahoo.com>
2021-09-10 18:34:12 +0200

    Convert BookShelf to a functional component

--- src/Components/BookShelf.js
+++ src/Components/BookShelf.js
@@ -1,32 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "./Book";
 import "./BookShelf.css";
 import PropTypes from "prop-types";
 
-class BookShelf extends Component {
-    render() {
-        const { books, shelf, onBookShelfChange } = this.props;
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelf}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books.map((book) => (
-                            <Book
-                                key={book.id}
-                                id={book.id}
-                                book={book}
-                                onStatusChange={(book, newShelf) => {
-                                    onBookShelfChange(book, newShelf);
-                                }}
-                            />
-                        ))}
-                    </ol>
-                </div>
+const BookShelf = (props) => {
+    const { books, shelf, onBookShelfChange } = props;
+    const booksOnShelf = books.map((book) => (
+        <Book
+            key={book.id}
+            id={book.id}
+            book={book}
+            onStatusChange={(book, newShelf) =>
+                onBookShelfChange(book, newShelf)
+            }
+        />
+    ));
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelf}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">{booksOnShelf}</ol>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 BookShelf.propTypes = {
     books: PropTypes.array.isRequired,
