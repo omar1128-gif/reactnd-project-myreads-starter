e32f5a358a4b57eda8a1860a906e89b7e83ba088
omar1128-gif <o_fathy28@yahoo.com>
2021-09-10 18:38:25 +0200

    Convert Book to a functional component

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,70 +1,68 @@
-import React, { Component } from "react";
+import React from "react";
 import "./Book.css";
 import PropTypes from "prop-types";
 
-class Book extends Component {
-    render() {
-        const { book, id, onStatusChange } = this.props;
-        return (
-            <li key={id}>
-                <div className="book">
-                    <div className="book-top">
-                        {book.imageLinks !== undefined ? (
-                            <div
-                                className="book-cover"
-                                style={{
-                                    width: 128,
-                                    height: 192,
-                                    backgroundImage: `url(${
-                                        book.imageLinks.thumbnail
-                                    })`,
-                                }}
-                            />
-                        ) : (
-                            <div
-                                className="book-cover"
-                                style={{
-                                    width: 128,
-                                    height: 192,
-                                    backgroundColor: "white",
-                                }}
-                            />
-                        )}
+const Book = (props) => {
+    const { book, id, onStatusChange } = props;
+    return (
+        <li key={id}>
+            <div className="book">
+                <div className="book-top">
+                    {book.imageLinks !== undefined ? (
+                        <div
+                            className="book-cover"
+                            style={{
+                                width: 128,
+                                height: 192,
+                                backgroundImage: `url(${
+                                    book.imageLinks.thumbnail
+                                })`,
+                            }}
+                        />
+                    ) : (
+                        <div
+                            className="book-cover"
+                            style={{
+                                width: 128,
+                                height: 192,
+                                backgroundColor: "white",
+                            }}
+                        />
+                    )}
 
-                        <div className="book-shelf-changer">
-                            <select
-                                onChange={(event) =>
-                                    onStatusChange(book, event.target.value)
-                                }
-                                value={book.shelf ? book.shelf : "none"}
-                            >
-                                <option value="move" disabled>
-                                    Move to...
-                                </option>
-                                <option value="currentlyReading">
-                                    Currently Reading
-                                </option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{book.title}</div>
-                    <div className="book-authors">
-                        {Array.isArray(book.authors) ? (
-                            book.authors.map((author) => (
-                                <p key={author}>{author}</p>
-                            ))
-                        ) : (
-                            <p>{book.authors}</p>
-                        )}
+                    <div className="book-shelf-changer">
+                        <select
+                            onChange={(event) =>
+                                onStatusChange(book, event.target.value)
+                            }
+                            value={book.shelf ? book.shelf : "none"}
+                        >
+                            <option value="move" disabled>
+                                Move to...
+                            </option>
+                            <option value="currentlyReading">
+                                Currently Reading
+                            </option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
                     </div>
                 </div>
-            </li>
-        );
-    }
-}
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">
+                    {Array.isArray(book.authors) ? (
+                        book.authors.map((author) => (
+                            <p key={author}>{author}</p>
+                        ))
+                    ) : (
+                        <p>{book.authors}</p>
+                    )}
+                </div>
+            </div>
+        </li>
+    );
+};
 
 Book.propTypes = {
     book: PropTypes.object.isRequired,
